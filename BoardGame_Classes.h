dd7daf0c965e189d720b362eacd8394a7d24a72d
Treka10 <20230020@stud.fci-cu.edu.eg>
2024-12-15 18:06:25 +0300

    Add files via upload

--- BoardGame_Classes.h
+++ BoardGame_Classes.h
@@ -13,10 +13,23 @@ protected:
     int n_moves = 0;
 
 public:
+
+
+    /// Return true if move is valid and put it on the board
+    /// within board boundaries in an empty cell
+    /// Return false otherwise
     virtual bool update_board(int x, int y, T symbol) = 0;
+
+    /// Display the board and the pieces on it
     virtual void display_board() = 0;
+
+    /// Returns true if there is any winner
     virtual bool is_win() = 0;
+
+    /// Return true if all moves are done and no winner
     virtual bool is_draw() = 0;
+
+    /// Return true if the game is over
     virtual bool game_is_over() = 0;
 };
 
@@ -25,24 +38,32 @@ class Player {
 protected:
     string name;
     T symbol;
-    Board<T>* boardPtr;
-
+    Board<T>* boardPtr;  // Pointer to the board
 public:
+    /// Two constructors to initiate players
+    /// Give the player a symbol to use in playing
+    /// It can be X, O, or others
+    /// Optionally, you can give them a name
     Player(string n, T symbol);
-    Player(T symbol);
+    Player(T symbol); // For computer players
+
     virtual void getmove(int& x, int& y) = 0;
     T getsymbol();
     string getname();
     void setBoard(Board<T>* b);
 };
 
+/// This class represents a random computer player
+/// that generates random positions x, y
+/// If invalid, the game manager asks to regenerate
 template <typename T>
 class RandomPlayer : public Player<T> {
 protected:
     int dimension;
-
 public:
+    // Take a symbol and pass it to the parent
     RandomPlayer(T symbol);
+    // Generate a random move
     virtual void getmove(int& x, int& y) = 0;
 };
 
@@ -51,11 +72,14 @@ class GameManager {
 private:
     Board<T>* boardPtr;
     Player<T>* players[2];
-
 public:
     GameManager(Board<T>*, Player<T>* playerPtr[2]);
+
     void run();
 };
+
+
+//--------------------------------------- IMPLEMENTATION
 
 #include <iostream>
 using namespace std;
@@ -70,6 +94,7 @@ GameManager<T>::GameManager(Board<T>* bPtr, Player<T>* playerPtr[2]) {
 template <typename T>
 void GameManager<T>::run() {
     int x, y;
+
     boardPtr->display_board();
 
     while (!boardPtr->game_is_over()) {
@@ -91,34 +116,62 @@ void GameManager<T>::run() {
     }
 }
 
+
+using namespace std;
+// Constructor for Player with a name and symbol
 template <typename T>
 Player<T>::Player(std::string n, T symbol) {
     this->name = n;
     this->symbol = symbol;
 }
 
+// Constructor for Player with only a symbol (e.g., for computer players)
 template <typename T>
 Player<T>::Player(T symbol) {
     this->name = "Computer";
     this->symbol = symbol;
 }
 
+// Constructor for RandomPlayer, passing the symbol to the parent Player class
 template <typename T>
 RandomPlayer<T>::RandomPlayer(T symbol) : Player<T>(symbol) {}
 
+// Getter for player's name
 template <typename T>
 std::string Player<T>::getname() {
     return this->name;
 }
 
+// Getter for player's symbol
 template <typename T>
 T Player<T>::getsymbol() {
     return this->symbol;
 }
 
+// Method to set the board pointer in the Player class
 template <typename T>
 void Player<T>::setBoard(Board<T>* b) {
     this->boardPtr = b;
 }
 
-#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //_BOARDGAME_CLASSES_H
+
